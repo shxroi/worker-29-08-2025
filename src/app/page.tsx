58dd68f83a835230c2af952268fed9eab6f94c5d
shxroi <rakhaboy2590@gmail.com>
2025-08-31 15:23:36 +0800

    Day 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
-import ProfileBar from "../components/home/ProfileBar";
+import Navbar from "../components/steam/Navbar";
+import NavbarSearch from "@/components/steam/NavSearch";
+import FeaturedCarousel from "@/components/steam/FeaturedCarousel";
+import Footer from "@/components/steam/Footer";
 
 
 export default function Home() {
   return (
-    <div>
-      <ProfileBar />
-    </div>
+    <>
+      <Navbar />
+      <NavbarSearch />
+      <div className="relative w-full h-[250px] sm:h-[200px] md:h-[200px] lg:h-[400px]">
+        <Image
+          className="object-cover"
+          src="/page_bg.jpg"
+          alt="Page Background"
+          fill
+          priority
+        />
+      </div>
+      <FeaturedCarousel />
+      <Footer />
+    </>
   );
 }
